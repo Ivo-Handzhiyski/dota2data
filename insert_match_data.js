3334ed27e4d2294f0790b6b22c12488c0ffc6301
Ivo Stefanov Handzhiyski <ivo.handzhiyski@blankfactor.com>
2023-04-18 16:00:58 +0300

    set up to get matches data

--- insert_match_data.js
+++ insert_match_data.js
@@ -15,3 +15,11 @@ getData('https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/v1/?access
   console.log(data.result.picks_bans[0])
   console.log(data.result.players[0])
 })
+// GET MATCH BY SEQ NUM TODO FOR LOOP AND INCREASE EVERY START WITH 100
+getData('https://api.steampowered.com/IDOTA2Match_570/GetMatchHistoryBySequenceNum/v1/?access_token=55e1dd21804a3f3f7a49f7dfdf2c1924&matches_requested=1000000').then((data) => {
+
+  // INSERT ITEMS
+  console.log(data.result.matches[0])
+  console.log(data.result.matches[0].picks_bans[0])
+  console.log(data.result.matches[0].players[0])
+})
